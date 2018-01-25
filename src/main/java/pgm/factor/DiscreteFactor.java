f75b8d8bfabbda2bde787bfc5b35f862ea191edd
alex <alexander.zakshevsky@itc-engineering.com>
2018-01-25 21:58:20 +0100

    DiscreteFactor refactoring +marginalize operation

--- src/main/java/pgm/factor/DiscreteFactor.java
+++ src/main/java/pgm/factor/DiscreteFactor.java
@@ -3,20 +3,22 @@ package pgm.factor;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Set;
-import java.util.TreeSet;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public final class DiscreteFactor {
 
     private static final DiscreteFactor EMPTY_FACTOR = new DiscreteFactor();
 
-    private final Set<Integer> variables;
-    private final List<Integer> cardinalities;
+    private final Map<Integer, Integer> varToCardMap;
     private final List<Double> values;
 
     public static class Builder {
@@ -31,18 +33,24 @@ public final class DiscreteFactor {
             this.vals = new ArrayList<>();
         }
 
-        public Builder variables(int variable, int ... variables) {
+        public Builder var(int variable, int ... variables) {
             this.vars.add(variable);
             if (variables != null && variables.length > 0) {
                 for (int v : variables) {
                     this.vars.add(v);
                 }
+                if (this.vars.size() != variables.length + 1) {
+                    throw new IllegalArgumentException("Variables must be unique");
+                }
             }
             return this;
         }
 
-        public Builder variables(Collection<Integer> variables) {
+        public Builder var(Collection<Integer> variables) {
             this.vars.addAll(variables);
+            if (this.vars.size() != variables.size()) {
+                throw new IllegalArgumentException("Variables must be unique");
+            }
             return this;
         }
 
@@ -61,7 +69,7 @@ public final class DiscreteFactor {
             return this;
         }
 
-        public Builder values(double ... values) {
+        public Builder val(double ... values) {
             if (values == null) {
                 throw new IllegalArgumentException("Values cannot be null");
             }
@@ -71,7 +79,7 @@ public final class DiscreteFactor {
             return this;
         }
 
-        public Builder values(Collection<Double> values) {
+        public Builder val(Collection<Double> values) {
             if (values == null) {
                 throw new IllegalArgumentException("Values cannot be null");
             }
@@ -81,87 +89,177 @@ public final class DiscreteFactor {
 
         public DiscreteFactor build() {
             if (vars.size() != cards.size()) {
-                throw new IllegalStateException("Dimensionality mismatch beween random variables and cardinalities");
+                throw new IllegalArgumentException("Dimensionality mismatch beween variables and cardinalities");
             }
-            if (getCardinality(cards) != vals.size()) {
-                throw new IllegalStateException("Dimensionality mismatch beween values and cardinalities");
+            if (getCardSize(cards) != vals.size()) {
+                throw new IllegalArgumentException("Dimensionality mismatch beween values and cardinalities");
             }
+            if (cards.stream().anyMatch(c -> c == null || c <= 0)) {
+                throw new IllegalArgumentException("Cardinality must be not null and strictly positive");
+            }
+            if (vals.stream().anyMatch(v -> v == null || v < 0)) {
+                throw new IllegalArgumentException("Value must be not null and non negative");
+            }
+            if (vars.contains(null)) {
+                throw new IllegalArgumentException("Variable cannot be null");
+            }
+
             if (vars.size() == 0 && cards.size() == 0 && vals.size() == 0) {
                 return EMPTY_FACTOR;
             }
+
             return new DiscreteFactor(this);
         }
     }
 
+    private static class Assignment {
+
+        private Set<Integer> randomVars;
+        private Collection<Integer> cardinalities;
+        private Collection<Integer> indexes;
+
+        private Assignment(Collection<Integer> indexes, Set<Integer> randomVars, Collection<Integer> cardinalities) {
+            this.indexes = indexes;
+            this.randomVars = randomVars;
+            this.cardinalities = cardinalities;
+        }
+
+        private Assignment mapAssignment(Set<Integer> mapVars) {
+            List<Integer> randomVarList = new ArrayList<>(randomVars);
+            List<Integer> mapIndexes = new ArrayList<>();
+            List<Integer> mapCardinalities = new ArrayList<>();
+            List<Integer> indexList = new ArrayList<>(indexes);
+            List<Integer> cardList = new ArrayList<>(cardinalities);
+
+            for (int mapVar : mapVars) {
+                int i = randomVarList.indexOf(mapVar);
+                if (i >= 0) {
+                    mapIndexes.add(indexList.get(i));
+                    mapCardinalities.add(cardList.get(i));
+                }
+            }
+
+            return new Assignment(mapIndexes, mapVars, mapCardinalities);
+        }
+
+        private int toIndex() {
+            if (indexes == null || indexes.size() == 0) {
+                throw new IllegalArgumentException("Assignment cannot be empty");
+            }
+            if (cardinalities == null || cardinalities.size() == 0) {
+                throw new IllegalArgumentException("Cardinalities cannot be empty");
+            }
+            if (indexes.size() != cardinalities.size()) {
+                throw new IllegalArgumentException("invalid assignment");
+            }
+
+            List<Integer> indexList = new ArrayList<>(indexes);
+            List<Integer> cardList = new ArrayList<>(cardinalities);
+
+            for (int i = 0; i < indexList.size(); i++) {
+                int assignmentIndex = indexList.get(i);
+                if (assignmentIndex <= 0 || assignmentIndex > cardList.get(i)) {
+                    throw new IllegalArgumentException("Invalid assignment");
+                }
+            }
+
+            int cumprod = 1;
+            int assignmentIndex = indexList.get(0) - 1;
+
+            for (int i = 1; i < cardList.size(); i++) {
+                cumprod *= cardList.get(i - 1);
+                assignmentIndex += cumprod * (indexList.get(i) - 1);
+            }
+
+            return assignmentIndex;
+        }
+
+        private static Assignment toAssignment(int index, Set<Integer> variables, Collection<Integer> cardinalities) {
+            if (index < 0) {
+                throw new IllegalArgumentException("Index cannot be less than zero");
+            }
+            if (cardinalities == null || cardinalities.size() == 0) {
+                throw new IllegalArgumentException("Cardinalities cannot be empty");
+            }
+
+            List<Integer> cardList = new ArrayList<>(cardinalities);
+
+            int cumprod = 1;
+            List<Integer> indexes = new ArrayList<>(cardinalities.size());
+            indexes.add(index % cardList.get(0) + 1);
+
+            for (int i = 1; i < cardinalities.size(); i++) {
+                cumprod *= cardList.get(i - 1);
+                indexes.add((index / cumprod) % cardList.get(i) + 1);
+            }
+
+            return new Assignment(indexes, variables, cardinalities);
+        }
+    }
+
+    private static class AssignmentIterator implements Iterable<Assignment> {
+
+        private final Collection<Integer> cardinalities;
+        private final Set<Integer> variables;
+        private final int size;
+        private int i = 0;
+
+        private AssignmentIterator(Set<Integer> variables, Collection<Integer> cardinalities)  {
+            this.variables = variables;
+            this.cardinalities = cardinalities;
+            this.size = getCardSize(cardinalities);
+        }
+
+        @Override
+        public Iterator<Assignment> iterator() {
+            return new Iterator<Assignment>() {
+
+                @Override
+                public boolean hasNext() {
+                    return i < size;
+                }
+
+                @Override
+                public Assignment next() {
+                    if (!hasNext()) {
+                        throw new NoSuchElementException();
+                    }
+
+                    Assignment result = Assignment.toAssignment(i, variables, cardinalities);
+                    i++;
+                    return result;
+                }
+            };
+        }
+    }
+
     private DiscreteFactor() {
-        variables = Collections.emptySet();
-        cardinalities = Collections.emptyList();
+        varToCardMap = Collections.emptyMap();
         values = Collections.emptyList();
     }
 
     private DiscreteFactor(Builder builder) {
-        variables = Collections.unmodifiableSet(builder.vars);
-        cardinalities = Collections.unmodifiableList(builder.cards);
-        values = Collections.unmodifiableList(builder.vals);
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        int i = 0;
+        for (int variable : builder.vars) {
+            map.put(variable, builder.cards.get(i));
+            i++;
+        }
+
+        this.varToCardMap = Collections.unmodifiableMap(map);
+        this.values = Collections.unmodifiableList(builder.vals);
     }
 
-    private static int getCardinality(Collection<Integer> cardinalities) {
+    private static AssignmentIterator assignments(Map<Integer, Integer> varToCardMap) {
+        return new AssignmentIterator(varToCardMap.keySet(), varToCardMap.values());
+    }
+
+    private static int getCardSize(Collection<Integer> cardinalities) {
         if (cardinalities.size() == 0) {
             return 0;
         }
 
-        int cardinality = 1;
-        for (int c : cardinalities) {
-            cardinality *= c;
-        }
-
-        return cardinality;
-    }
-
-    private static int assignmentToIndex(List<Integer> assignment, Collection<Integer> cardinalities) {
-        if (assignment.size() != cardinalities.size()) {
-            throw new IllegalArgumentException("invalid assignment");
-        }
-
-        List<Integer> cardinalityList = new ArrayList<>(cardinalities);
-
-        for (int i = 0; i < assignment.size(); i++) {
-            int assignmentIndex = assignment.get(i);
-            if (assignmentIndex <= 0 || assignmentIndex > cardinalityList.get(i)) {
-                throw new IllegalArgumentException("Invalid assignment");
-            }
-        }
-
-        int cumprod = 1;
-        int assignmentIndex = assignment.get(0) - 1;
-
-        for (int i = 1; i < cardinalityList.size(); i++) {
-            cumprod *= cardinalityList.get(i - 1);
-            assignmentIndex += cumprod * (assignment.get(i) - 1);
-        }
-
-        return assignmentIndex;
-    }
-
-    private static List<Integer> indexToAssignment(int index, Collection<Integer> cardinalities) {
-        if (index < 0) {
-            throw new IllegalArgumentException("Index cannot be less than zero");
-        }
-        if (cardinalities == null || cardinalities.size() == 0) {
-            throw new IllegalArgumentException("Empty cardinialities");
-        }
-
-        int cumprod = 1;
-        List<Integer> cardinalityList = new ArrayList<>(cardinalities);
-        List<Integer> assignment = new ArrayList<>(cardinalities.size());
-        assignment.add(index % cardinalityList.get(0) + 1);
-
-        for (int i = 1; i < cardinalities.size(); i++) {
-            cumprod *= cardinalityList.get(i - 1);
-            assignment.add((index / cumprod) % cardinalityList.get(i) + 1);
-        }
-
-        return assignment;
+        return cardinalities.stream().reduce(1, (a, b) -> a * b);
     }
 
     public DiscreteFactor product(DiscreteFactor ... factors) {
@@ -169,63 +267,44 @@ public final class DiscreteFactor {
             throw new IllegalArgumentException("Factors cannot be empty");
         }
 
-        List<DiscreteFactor> factorList = new ArrayList<>();
-        factorList.add(this);
+        List<DiscreteFactor> allFactors = new ArrayList<>();
+        allFactors.add(this);
+        Collections.addAll(allFactors, factors);
 
-        TreeSet<Integer> union = new TreeSet<>(variables);
-        for (DiscreteFactor factor : factors) {
-            factorList.add(factor);
-            union.addAll(factor.variables);
+        allFactors = allFactors.stream().filter(factor -> factor != EMPTY_FACTOR).collect(Collectors.toList());
+        if (allFactors.size() == 0) {
+            return EMPTY_FACTOR;
+        }
+        else if (allFactors.size() == 1) {
+            return allFactors.get(0);
         }
 
-        List<Integer> unionVarList = new ArrayList<>(union);
-        Map<Integer, Integer> cardMap = new HashMap<>();
-
-        List<List<Integer>> indexMapList = new ArrayList<>();
-        for (DiscreteFactor factor : factorList) {
-            List<Integer> varIndexes = new ArrayList<>(factor.variables.size());
-            int varIndex = 0;
-            for (int factorVar : factor.variables) {
-                int unionVarIndex = unionVarList.indexOf(factorVar);
-                if (unionVarIndex >= 0) {
-                    cardMap.put(unionVarIndex, factor.cardinalities.get(varIndex));
-                    varIndexes.add(unionVarIndex);
+        Map<Integer, Integer> union = new TreeMap<>();
+        for (DiscreteFactor factor : allFactors) {
+            for (int factorVar : factor.varToCardMap.keySet()) {
+                if (union.containsKey(factorVar) && !union.get(factorVar).equals(factor.varToCardMap.get(factorVar))) {
+                    throw new IllegalArgumentException("Dimensionality mismatch in factors");
                 }
-                if (varIndexes.size() == factor.variables.size()) {
-                    break;
-                }
-                varIndex++;
             }
 
-            indexMapList.add(varIndexes);
+            union.putAll(factor.varToCardMap);
         }
 
         List<Double> resultVals = new ArrayList<>();
-        int resultCard = getCardinality(cardMap.values());
 
-        for (int i = 0; i < resultCard; i++) {
-            List<Integer> assignment = indexToAssignment(i, cardMap.values());
-            double product = 1;
-
-            for (int j = 0; j < indexMapList.size(); j++) {
-                DiscreteFactor factor = factorList.get(j);
-                List<Integer> factorIndexMap = indexMapList.get(j);
-                List<Integer> factorAssignment = new ArrayList<>();
-
-                for (int varAssignmentIndex : factorIndexMap) {
-                    factorAssignment.add(assignment.get(varAssignmentIndex));
-                }
-                int index = assignmentToIndex(factorAssignment, factor.cardinalities);
-                product *= factor.values.get(index);
+        for (Assignment assignment : assignments(union)) {
+            double result = 1;
+            for (DiscreteFactor factor : allFactors) {
+                Assignment factorAssignment = assignment.mapAssignment(factor.variables());
+                result *= factor.values.get(factorAssignment.toIndex());
             }
-
-            resultVals.add(product);
+            resultVals.add(result);
         }
 
         return new Builder()
-            .variables(union)
-            .card(cardMap.values())
-            .values(resultVals)
+            .var(union.keySet())
+            .card(union.values())
+            .val(resultVals)
             .build();
     }
 
@@ -234,8 +313,50 @@ public final class DiscreteFactor {
         for (int index : indexes) {
             indexesList.add(index);
         }
-        int assignmentIndex = assignmentToIndex(indexesList, this.cardinalities);
-        return this.values.get(assignmentIndex);
+        Assignment assignment = new Assignment(indexesList, varToCardMap.keySet(), varToCardMap.values());
+        return this.values.get(assignment.toIndex());
+    }
+
+    public DiscreteFactor marginalize(int marginalizeVar, int ... marginalizeVars) {
+        Set<Integer> varSet = new LinkedHashSet<>();
+        varSet.add(marginalizeVar);
+        if (marginalizeVars != null) {
+            for (int margVar : marginalizeVars) {
+                varSet.add(margVar);
+            }
+        }
+        return marginalize(varSet);
+    }
+
+    public DiscreteFactor marginalize(Set<Integer> marginalizeVars) {
+        Map<Integer, Integer> margVarToCardMap = new TreeMap<>(varToCardMap);
+
+        margVarToCardMap.keySet().removeAll(marginalizeVars);
+        Set<Integer> margVars = margVarToCardMap.keySet();
+        Collection<Integer> margCards = margVarToCardMap.values();
+
+        if (margVars.size() == 0) {
+            throw new IllegalArgumentException("Resultant factor has empty scope");
+        }
+
+        AssignmentIterator assignments = assignments(varToCardMap);
+
+        int size = getCardSize(margCards);
+        List<Double> resultVals = new ArrayList<>(Collections.nCopies(size, 0d));
+
+        for (Assignment assignment : assignments) {
+            Assignment margAssignment = assignment.mapAssignment(margVars);
+            int i = margAssignment.toIndex();
+            int j = assignment.toIndex();
+            double val = resultVals.get(i) + values.get(j);
+            resultVals.set(i, val);
+        }
+
+        return new Builder()
+            .var(margVars)
+            .card(margCards)
+            .val(resultVals)
+            .build();
     }
 
     public List<Double> values() {
@@ -243,21 +364,19 @@ public final class DiscreteFactor {
     }
 
     public Set<Integer> variables() {
-        return new LinkedHashSet<>(variables);
+        return new LinkedHashSet<>(varToCardMap.keySet());
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Discreate Random Factor");
-        int i = 0;
-        for (int variable : variables) {
+        sb.append("Discrete Random Factor");
+        for (Map.Entry<Integer, Integer> entry : varToCardMap.entrySet()) {
             sb.append(" | ");
-            sb.append(variable);
-            sb.append(" (");
-            sb.append(cardinalities.get(i));
+            sb.append(entry.getKey());
+            sb.append("(");
+            sb.append(entry.getValue());
             sb.append(") ");
-            i++;
         }
         return sb.toString();
     }
@@ -272,7 +391,7 @@ public final class DiscreteFactor {
         }
 
         DiscreteFactor that = (DiscreteFactor) obj;
-        if (!Objects.equals(variables, that.variables)) {
+        if (!Objects.equals(varToCardMap, that.varToCardMap)) {
             return false;
         }
         if (values.size() != that.values.size()) {
@@ -289,6 +408,6 @@ public final class DiscreteFactor {
 
     @Override
     public int hashCode() {
-        return Objects.hash(variables);
+        return Objects.hash(varToCardMap);
     }
 }
